de524663b5076ea9e3c9643f9f2661b368090b12
DLasta <dario.lasta123@gmail.com>
2024-08-04 20:10:26 +0200

    pocni

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.layout')
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos alias consequuntur omnis iste? Optio autem
+            inventore nisi accusamus labore corrupti sint? Laboriosam explicabo similique voluptatibus velit esse
+            accusantium ratione nisi.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos alias
+            consequuntur omnis iste? Optio autem inventore nisi accusamus labore corrupti sint? Laboriosam explicabo
+            similique voluptatibus velit esse accusantium ratione nisi.Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Dignissimos alias consequuntur omnis iste? Optio autem inventore nisi accusamus labore corrupti sint?
+            Laboriosam explicabo similique voluptatibus velit esse accusantium ratione nisi. </p>
+    </div>
+@endsection
